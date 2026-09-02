02b7cac09a02f6a247f896d8c87545c82b0a582d
khadijahadouch345 <khadijahadouchi108@gmail.com>
2026-09-02 15:58:27 +0200

    les challenge de comdestion et boucles

--- script3.js
+++ script3.js
@@ -42,10 +42,84 @@ let delta =(nombre2*nombre2)-(4*nombre1*nombre3)
 //affichage 4
 let x1;
 let x2;
-let x3
+let x3;
 if(delta>0){
     x1=(-nombre2 - (delta* 1/2) )/(2*nombre1);
     x2=(-nombre2 + (delta* 1/2) )/(2*nombre1);
+    console.log(`les deux solutions sont:${x1} et ${x2}`);
 }else if(delta ==0){
-    x3=(-nombre2)/(2)
+    x3=(-nombre2)/(2*nombre1);
+    console.log(`la solution est unique:${x3}`);
+}else{
+    console.log(`il n y a pas de solution`);
+   
+
 }
+//challenge 5 
+let annee =prompt ("entre l annee");
+console.log('1.Mois ');
+console.log('2.Jours ');
+console.log('3.Heures ');
+console.log('4.Minutes ');
+console.log('5.Secondes ');
+let choix =  parseInt(prompt ('entre votre choix'));
+switch(choix){
+    case 1 :
+        console.log(`pour le mois est :${annee*12}`);
+        break;
+        case 2:
+            console.log(`pur le jours est :${annee*365}`);
+            break;
+            case 3:
+                console.log(` pour les heures est:${annee*24*365}`);
+                break;
+                case 4:
+                    console.log(`pour minutes est:${annee*60*24*365}`);
+                    break;
+                case 5:
+                    console.log(`pour secondes est:${annee*60*60*24*365}`);
+                    break;
+                default:
+                        console.log(`choix invalide`);
+
+}
+//challenge 6 
+let nmbr = prompt ("entrez un nombre ");
+if(nmbr>0){
+    console.log(`le nombre est positif`);
+
+}else if(nmbr<0){
+    console.log(`la nombre est negative`);
+}else {
+    console.log(`le nombre est egal a zero`);
+}
+//challenge 7 
+let   alphb = prompt ("entre une lettre");
+if(alphb.charCodeAt(0) >=65 && alphb.charCodeAt(0) <=90){
+    console.log(`la lettre est majuscule`);
+}else {
+    console.log(`la lettre est minuscule`);
+
+    
+}
+
+//challenge 8 
+let notes = prompt ("entre votre note ");
+if(notes<10){
+    console.log(`recale `);
+
+}else if(notes>=10 && notes<12){
+    console.log(`passable`);
+ }else if(notes>=12 && notes<14){
+    console.log(`assez bien`);
+}else if(notes>=14 && notes<16){
+    console.log(`bien`);
+    
+}else {
+
+    console.log(`tres bien`);
+
+}
+//challenge 9
+let crt = prompt ("entre un caractere")
+if(crt)
