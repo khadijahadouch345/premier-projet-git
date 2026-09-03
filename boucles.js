9e7afcb21ea71feed133c19daa90855c65f04d0b
khadijahadouch345 <khadijahadouchi108@gmail.com>
2026-09-03 10:10:25 +0200

    challenge 4

--- boucles.js
+++ boucles.js
@@ -1,36 +1,48 @@
 const prompt = require('prompt-sync')();
-// 1. Saisie du nombre
-let n = parseInt(prompt("Entrez le nombre pour la table de multiplication :"));
+// // 1. Saisie du nombre
+// let n = parseInt(prompt("Entrez le nombre pour la table de multiplication :"));
 
-console.log(`Table de multiplication de ${n} :`);
+// console.log(`Table de multiplication de ${n} :`);
 
-// 2. Boucle for de 1 à 10
-for (let i = 1; i <= 10; i++) {
-    let resultat = n * i;
+// // 2. Boucle for de 1 à 10
+// for (let i = 1; i <= 10; i++) {
+//     let resultat = n * i;
     
-    // 3. Affichage du calcul
-    console.log(`${n} * ${i} = ${resultat}`);
-}
-// 1. Saisie de n
-let n = parseInt(prompt("Entrez un entier positif pour la factorielle :"));
+//     // 3. Affichage du calcul
+//     console.log(`${n} * ${i} = ${resultat}`);
+// }
+// // 1. Saisie de n
+// let n = parseInt(prompt("Entrez un entier positif pour la factorielle :"));
 
-// 2. Initialisation à 1
-let factorielle = 1;
+// // 2. Initialisation à 1
+// let factorielle = 1;
 
-// 3. Boucle de 1 à n
-for (let i = 1; i <= n; i++) {
-    factorielle *= i; 
-}
+// // 3. Boucle de 1 à n
+// for (let i = 1; i <= n; i++) {
+//     factorielle *= i; 
+// }
 
-console.log(`${n}! = ${factorielle}`);
-// chalenge3 
-let n = parseInt(prompt("Entre un nombre entie "));
-let somme=0;
-let i;
-for(i=0 ; i<=n ;i++){
-    somme=somme+i;
+// console.log(`${n}! = ${factorielle}`);
+// // chalenge3 
+// let n = parseInt(prompt("Entre un nombre entie "));
+// let somme=0;
+// let i;
+// for(i=0 ; i<=n ;i++){
+//     somme=somme+i;
 
-}
-console.log(`La somme des nombresest : ${somme}`);
+// }
+// console.log(`La somme des nombresest : ${somme}`);
 //challenge 4 
-let 
+let nbr =prompt("entre un nombre");
+let i=1;
+let x=1;
+while(i<=nbr){
+    console.log(x);
+    x+=2;
+    i++;
+
+}
+//challenge 5
+
+
+
