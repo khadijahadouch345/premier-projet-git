1bed3c489b989129e89ecd5947356f0c017019c3
khadijahadouch345 <khadijahadouchi108@gmail.com>
2026-09-06 20:43:50 +0100

    les challege 10,11,12 et 13

--- script.js
+++ script.js
@@ -53,10 +53,54 @@ const prompt = require('prompt-sync')();
 // //affichage 7
 // console.log(`la moyenne pondereeest:${((nombre1*2 )+(nombre2*3)+(nombre3*5))/10}`);
 //challenge 8
-let nmb1 = prompt ("entre le premier nombre");
-let nmb2 = prompt("entre le deuxieme nombre");
-let nmb3 = prompt ("entre le troiseme nombre");
-//affichage 8 
-console.log(`la moyenne geometrique est:${(nmb1 * nmb2 * nmb3)**(1/3)}`);
+// let nmb1 = prompt ("entre le premier nombre");
+// let nmb2 = prompt("entre le deuxieme nombre");
+// let nmb3 = prompt ("entre le troiseme nombre");
+// //affichage 8 
+// console.log(`la moyenne geometrique est:${(nmb1 * nmb2 * nmb3)**(1/3)}`);
+// //challenge 9
+// let x1= prompt("entre la valeur de x1 : ");
+// let x2= prompt("entre lavaleur de x2 : ");
+// let y1= prompt("entre la valeur de y1: ");
+// let y2= prompt("entre lavaleur de y2 : ");
+// let z1= prompt("entre la valeur de z1 : ");
+// let z2= prompt("entre lavaleur de z2 : ");
+// let distance= (x2-x1)**2 +(y2-y1)**2 +(z2-z1);
+// //affichage 9
+// console.log(`la distance entre deux points dans un espace 3d est:${distance**0.5}`);
+// //challenge 10
+// let rayon= prompt("entre le rayon de la sphere:");
+// const pi=3.14;
+// //affichage 10
+// console.log(`le volume d'une sphère :${(4/3)*pi*(rayon)**3}`);
+// //challenge 11
+// let longueur=prompt("entre la langeur du rectangle:");
+// let largeur=prompt("entre la largure du rectangle:");cv 
+// let surface=longueur*largeur;
+// //affichage 11
+// console.log(`la surface d\'un rectangle:${surface}`);
+// //challenge 12
+// let nombre=Number(prompt("entre un nombre a quatre chiffres"));
+// let chiffre1=nombre%10;
+//  let rest=Math.floor(nombre/10);
+// let chiffre2=rest%10;
+//  rest=Math.floor(rest/10);
+// let chiffre3=rest%10;
+// rest=Math.floor(rest/10);
+// let chiffre4=rest%10;
+// //affichage 12
+// console.log(`Nombre en order inverse:${chiffre1}${chiffre2}${chiffre3}${chiffre4}`);
+//challenge13
+let nombreBiner=prompt("entre un nombre :");
+let bin="";
+while(nombreBiner >0 ){
+    let reste=nombreBiner%2;
+    bin=reste+bin;
+    nombreBiner=Math.floor(nombreBiner/2);
+}
+console.log(bin);
+
+
+
 
 
