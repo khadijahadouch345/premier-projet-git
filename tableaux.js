3c99dc1ac0f493b67df404d613e4cba704543c8c
khadijahadouch345 <khadijahadouchi108@gmail.com>
2026-09-07 17:40:57 +0200

    challenge tableaux,mini pojet

--- tableaux.js
+++ tableaux.js
@@ -1,2 +1,19 @@
 const prompt = require('prompt-sync')();
-//challenge 3
+// //challenge 3
+// let nombre=Number(prompt("entre le nombre deelement de tableaux"))
+// let tableu=[];
+// let somme = 0;
+// for(let i=0 ; i<nombre;i++){
+//     tableu.push(Number(prompt(`entre les valeur detableaux `)));
+// somme+=tableu[i];
+// }
+// console.log(somme);
+//challenge 4
+let nombre=Number(prompt("entre le nombre deelement de tableaux"))
+let tableu=[];
+for(let i=0 ; i<nombre;i++){
+    tableu.push(Number(prompt(`entre les valeur detableaux `)));
+somme+=tableu[i];
+}
+console.log(somme);
+//challenge 
